8a9c013ce7d4c4c5a4eb6db9db45e7f8c5a75cc4
vectorbr4vo <55299520+vectorbr4vo@users.noreply.github.com>
2019-09-19 14:28:17 -0700

    Initial commit

---
+++ A/src/niencraft1.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        int y = x + 5;
+        x = 20;
+
+        boolean lightsOn = true;
+        String firstName = "Paul";
+        String lastName = "Zaremba";
+        int age = 34;
+
+        String fullName = firstName + " " + lastName + " age: " + age;
+        String greet = "hellos";
+        System.out.println(greet.substring(1,3));
+        System.out.println(greet.indexOf("1"));
+
+
+
+
+    }
+}
